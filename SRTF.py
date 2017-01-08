c8e949621fc53fd77d9a4b2c8e522fe3833f8ae0
MariamKiran1996 <mariamkiran1996@gmail.com>
2017-01-08 23:09:43 +0500

    Add files via upload

--- SRTF.py
+++ SRTF.py
@@ -1,38 +1,35 @@
-arr = []
-wait_time = 0
-n = int(input('Enter the No. of Process : '))
+array = []
+wait_sum = 0.0
+total_burst_time = 0
+service_time = []
+waiting_time = []
+turnaround_time=[]
+process_number = int(input('Enter the number of Processes : '))
 
-for i in range(n):
-    arr.append([])
-    #print (' ')
-    arr[i].append(input('Enter Process Name : ' ))
-    arr[i].append(int(input('Enter Arrival Time ')))
-    arr[i].append(int(input('Enter Burst Time :')))
-    #print(' ')
+for i in range(process_number):
+    array.append([])
+    array[i].append(input('Process Name : ' ))
+    array[i].append(int(input('Process Arrival Time ')))
+    array[i].append(int(input('Process Burst Time :')))
+    print('\n')
+array.sort(key = lambda array:(array[1]))
 
+for i in range(process_number):
+    total_burst_time += array[i][2]
 
-arr.sort(key = lambda arr:(arr[1],arr[2]))
+service_time.append[array[0][1]]
+waiting_time.append(service_time[0] - array[0][1])
+turnaround_time.append(waiting_time[0]+array[0][2])
+j = 1
 
-
-total = 0
-for i in range(n):
-    total += arr[i][2]
-
-wait = []
-j = 0
-service = []
-service[0]=0
-service[1]=0
-
-while (j<=total):
-    service.append ( service[j] + arr[j][2] )
-    wait.append(service[j] - arr[j][1])
-    wait_time += wait[j]
+while j<=total_burst_time:
+    service_time.append(service_time[j-1] + array[j-1][2] )
+    waiting_time.append(service_time[j] - array[j][1])
+    turnaround_time.append(waiting_time[j]+array[j][2])
     j += 1
 
-print ('Process Name \tArrival Time \t Burst Time  \t Waiting Time')
-for i in range(n):
-    print(arr[i][0], '\t\t\t\t', arr[i][1], '\t\t\t\t', arr[i][2], '\t\t\t\t', wait[i])
-
-print ('Total Waiting Time : ',wait_time)
-print ('Average Waiting Time : ',(wait_time/n))
+print('Process Name \tArrival Time \t Burst Time  \t Waiting Time \t TurnAround Time')
+for i in range(process_number):
+    print(array[i][0], '\t\t\t\t', array[i][1], '\t\t\t\t', array[i][2], '\t\t\t\t', waiting_time[i], '\t\t\t\t', turnaround_time[i])
+    wait_sum += waiting_time[i]
+print ('Average Waiting Time : ',(wait_sum/(process_number*1.0)))
