a4c5e7290092c2c2ce46884ddc833807a947c119
MariamKiran1996 <mariamkiran1996@gmail.com>
2017-01-08 21:25:58 +0500

    Add files via upload

--- RR.py
+++ RR.py
@@ -1,52 +1,50 @@
-arr = []
-wait_time = 0
-n = int(input('Enter the No. of Process : '))
-
-for i in range(n):
-    arr.append([])
-    print(' ')
-    arr[i].append(input('Enter Process Name : ' ))
-    arr[i].append(int(input('Enter Arrival Time :')))
-    arr[i].append(int(input('Enter Burst Time :')))
-
-
-time_slice = int(input('Enter the time slice for each Process : '))
-
-arr.sort(key = lambda arr:(arr[1],arr[n]))
-
+array = []
+wait_sum = 0.0
 total = 0
-wait = []
-finish = []
-dup = []
+waiting_time = []
+finish_time = []
+burst_time_counter = []
+turnaround_time=[]
 
-for i in range(n):
-    finish.append(0)
-    total += arr[i][2]
-    dup.append(arr[i][2])
-    wait.append(0)
+process_number = int(input('Enter the number of processes : '))
 
+for i in range(process_number):
+    array.append([])
+    array[i].append(input('Process Name : ' ))
+    array[i].append(int(input('Process Arrival Time :')))
+    array[i].append(int(input('process Burst Time :')))
+    print('\n')
+
+time_slice = int(input('Enter the time quantum for each process : '))
+array .sort(key = lambda array :(array [1],array [process_number]))
+
+for i in range(process_number):
+    finish_time.append(0)
+    total += array [i][2]
+    burst_time_counter.append(array [i][2])
+    waiting_time.append(0)
 
 j = 0
 l = 0
 
 while (l<total):
-    j = j % n
+    j = j % process_number
     for k in range(time_slice):
-        if dup[j] != 0:
-            dup[j] -= 1
+        if burst_time_counter[j] != 0:
+            burst_time_counter[j] -= 1
             l += 1
-            if dup[j] == 0:
-                finish[j] = l + arr[j][1]
+            if burst_time_counter[j] == 0:
+                finish_time[j] = l + array [j][1]
                 break
     j += 1
 
-for i in range(n):
-    wait[i] = finish[i] - arr[i][1] - arr[i][2]
+for i in range(process_number):
+    waiting_time[i] = finish_time[i] - array[i][1] - array[i][2]
+    turnaround_time.append(waiting_time[i]+array[i][2])
 
-print ('Process Name \tArrival Time \t Burst Time \t  Waiting Time')
-for i in range(n):
-    print (arr[i][0], '\t\t\t\t', arr[i][1], '\t\t\t\t', arr[i][2], '\t\t\t\t', wait[i])
-    wait_time += wait[i]
+print ('Process Name \tArrival Time \t Burst Time \t  Waiting Time \t TurnAround Time')
+for i in range(process_number):
+    print (array[i][0], '\t\t\t\t', array[i][1], '\t\t\t\t', array[i][2], '\t\t\t\t', waiting_time[i], '\t\t\t\t', turnaround_time[i])
+    wait_sum += waiting_time[i]
 
-print ('Total Waiting Time : ',wait_time)
-print ('Average Waiting Time : ',(wait_time/n))
+print ('Average Waiting Time : ',(wait_sum/process_number))
